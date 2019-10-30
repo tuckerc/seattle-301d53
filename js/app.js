a1b369ce7edf2e6fd21665af08f1e877b2ce1daf
Chase Tucker <tuck8106@gmail.com>
2019-10-30 12:29:28 -0700

    lab 2 filter working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
+'use strict';
+
 $(function() {
   $.get('data/images.json')
     .then(imageLoader => {
       imageLoader.forEach(image => {
+        // add images to main
         $('main').append(`<h2>${image.title}</h2>`);
         $('main').append(`<img src='${image.image_url}' alt='${image.description}'></img>`);
         $('main').append(`<p>${image.description}</p>`);
+        // populate select
+        $('select').append(`<option value='${image.keyword}'>${image.keyword}</option>`);
       })
     })
+  $('select').on('change',() => {
+    $('main *').remove();
+    $.get('data/images.json')
+    .then(imageLoader => {
+      imageLoader.forEach(image => {
+        console.log(`image.keyword: ${image.keyword}`);
+        console.log(`$('select').first().value ${$('select').first().val()}`);
+        if(image.keyword === $('select').first().val()) {
+          // add images to main
+          $('main').append(`<h2>${image.title}</h2>`);
+          $('main').append(`<img src='${image.image_url}' alt='${image.description}'></img>`);
+          $('main').append(`<p>${image.description}</p>`);
+        }
+      })
+    })
+  });
 });
